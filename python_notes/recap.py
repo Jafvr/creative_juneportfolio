dfd1ece450afbf2801c575ca9ffca80bf84e679b
Jafar_AD@hotmail.com <Jafar_AD@hotmail.com>
2022-06-27 17:14:05 +0100

    udated python notes

--- python_notes/recap.py
+++ python_notes/recap.py
@@ -1,5 +1,21 @@
+from concurrent.futures.thread import _threads_queues
+from operator import truediv
+import string
+
+
 print("this is my file")
 
 greeting = "welcome everyone"
 
 print(greeting)
+
+# git add .
+# git comit -m "a message"
+# git push -u origin main
+
+# print (1234+4) this is a integer/ whole number
+# print (1234)this is a string
+# print(12.34) this is a float
+# print (True) oneof the boolean data values
+# print(False one of the booleans)
+# print (None) none - blank/null data
