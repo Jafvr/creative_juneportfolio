de9989a1110e53cc2f0c3a17923becb7b66c9ad2
Jafar_AD@hotmail.com <Jafar_AD@hotmail.com>
2022-06-28 13:02:50 +0100

    python notes for lists etc

--- python_notes/recap.py
+++ python_notes/recap.py
@@ -1,21 +1,149 @@
-from concurrent.futures.thread import _threads_queues
-from operator import truediv
-import string
+# from concurrent.futures.thread import _threads_queues
+# from operator import truediv
+# import string
+# from traceback import print_tb
 
 
-print("this is my file")
+# print("this is my file")
 
-greeting = "welcome everyone"
+# greeting = "welcome everyone"
 
-print(greeting)
+# print(greeting)
 
-# git add .
-# git comit -m "a message"
-# git push -u origin main
+# # git add .
+# # git comit -m "a message"
+# # git push -u origin main
 
-# print (1234+4) this is a integer/ whole number
-# print (1234)this is a string
-# print(12.34) this is a float
-# print (True) oneof the boolean data values
-# print(False one of the booleans)
-# print (None) none - blank/null data
+# # print (1234+4) this is a integer/ whole number
+# # print (1234)this is a string
+# # print(12.34) this is a float
+# # print (True) oneof the boolean data values
+# # print(False) one of the booleans)
+# # print (None) none - blank/null data
+
+# print(len(greeting))
+
+# print(greeting[1])
+
+# print(greeting.upper())
+
+# print("HELLO.lwoer"())
+
+# print("hello EVERYONE. THIS is creative".capitalize())
+
+# print("the quick brown fox".count("o"))
+
+# print("the quick brown fox".find("t"))
+
+# print("the quick brown fox".replace(fox,brown))
+
+# print(len("The quick brown fox"))
+
+# # stripped=("The quick brown fox")
+
+# # my_name="Jafar"
+# # my_age=30
+# # student=True
+
+# # print(my_name, my_age,student)
+
+# # print("hellow my name is", my_name)
+
+# # print("I am", my_age)
+
+# # print("hellow my name is" + my_name)
+# # print("i am" + my_age)
+
+# # print("hellow my name is {} and i am {} years old".format (my_name, my_age)
+
+# # # f string, new and best for practice.
+
+# # print("hello my nameis {my_name} and I am{my_age} years old")
+
+# # print(1+2)
+# # print(3-2)
+# # print(3**3)
+# # print(3*3)
+# # print(6/2)
+# # print(16%3)
+
+# # balance=10
+# # print(balance)
+# # amount=200
+
+# # balance=amount+balance
+# # balance +=amount
+# # print(balance)
+
+# # answer=input("what is your name")
+# # print(answer)
+
+# # answer=input("what is your name\n")
+# # print(answer)
+
+# from this import d
+
+
+# music= "classical"
+
+# if music == "classical": 
+#     print("oh no it's classical")
+#     print("turn it off")
+#     print("i hate debusy")
+# elif music == "no music":
+#     print("put the radio on")
+# else:
+#     print("turn it up")
+
+# print("that was an if statement")
+
+# num=10
+# num2=20
+
+# if num > num2:
+#     print(f"{"num"} is bigger")
+#     elif num2 > num:
+#         print(f"{num2} is bigger)
+
+
+
+# place = "MCR"
+# wearther = "Cloudy"
+
+# if place == "MCR" and weather == "sunny":
+#     print("check again")
+# elif place == "MCR" and weather == "rain":
+#     print("what is isn't raining")
+# else:
+#     print:("obvs")
+
+
+# day = "saturday"
+
+# if day == "saturday"or day == "sunday":
+#     print("it's the weekend!")
+# else:
+#     print("when's the weekend?")
+
+# day = "monday"
+# bank_hol = True
+
+# if day== "saturday" or day=="sunday" or bank_hol:
+#     print("A day off")
+# else:
+#     print("off to innovate we go")
+
+
+# def light_switch():
+#     print("switching the lights")
+
+
+# def cash_with():
+#     print("you have withdrawn {amount} from {accum}")
+
+fav_song = [
+    "Ram Riddlz- Hey Mr Ramrod",
+    "Drake- Let's call it off",
+    "Abba- SOS"
+]
+print(fav_song)
