2456ab8a235e776512648afbbfb9060c898d89e2
Jafar_AD@hotmail.com <Jafar_AD@hotmail.com>
2022-06-30 11:52:47 +0100

    Frontend notes added

---
+++ python_notes/dictionary_notes.py
@@ -0,0 +1,58 @@
+# # print("this is dictionaries")
+
+# # my_cat = {
+# #     "name":"salem",
+# #     "colour":"black",
+# #     "mood":"sassy"
+
+# # }
+
+# # my_dog = {
+# #     "name":"forest",
+# #     "breed":"fiction",
+# #     "mood":"hungry"
+
+
+# # }
+
+# # countries = {
+# #     "England":"London",
+# #     "france":"Paris",
+# #     "Germany":"Berlin",
+# #     "Spain":"Madrid",
+# #     "Italy":"Rome",
+
+
+# # }
+
+# # countries.setdefault("Ethopia","Addis Ababa")
+# # countries.setdefault("Peru","Lina")
+
+# # # print(countries) A way of printing the whole dictinary
+
+# # for i in countries.items():
+# #     print()
+
+# #     print(list(countries.items()))
+
+# #     for k, v in countries.items():
+# #         prin(k + ":"+v)
+
+
+# for x in countries.keys():
+#     print(x +" : " + countries[x])
+
+
+# countries.upate({"England":"English",
+#     "France":"French",
+
+
+
+# })
+# print(countries)
+
+
+
+# # .Del removies from lists or dictionary .pop too
+
+# # .append alows you to add
--- python_notes/levelup.py
+++ python_notes/levelup.py
@@ -15,82 +15,104 @@
 
 # # # balance +=deposit
 
-# # # print(f"you have {balance}")
+# # # # print(f"you have {balance}")
 
-# # # print("what is your name")
+# # # # print("what is your name")
 
-# # # name = input()
+# # # # name = input()
 
-# # # if name: 
-# # #     print(f"welcome {name} to innovate")
-# # # else:
-# # #     print("you did not submit a name")
-# # # # # if music =="classical"
-# # # # day="monday"
-# # # # bank_hol = True
+# # # # if name: 
+# # # #     print(f"welcome {name} to innovate")
+# # # # else:
+# # # #     print("you did not submit a name")
+# # # # # # if music =="classical"
+# # # # # day="monday"
+# # # # # bank_hol = True
 
-# # # if day =="saturday" or day=="sunday" or bank_hol:
-# # #     print("yay a day off")
-# # # else:
-# # #     print("off to innovate we go")
+# # # # if day =="saturday" or day=="sunday" or bank_hol:
+# # # #     print("yay a day off")
+# # # # else:
+# # # #     print("off to innovate we go")
 
-# # allowed= ["Andy","bob", "Carol","dean"]:
-# # name= input ("what is your name?")
+# # # allowed= ["Andy","bob", "Carol","dean"]:
+# # # name= input ("what is your name?")
 
-# # while name not in allowed:
-# #     print("your name isn't on the list")
-# #     print("try again")
-# #     name=input{"what is your name?"}
+# # # while name not in allowed:
+# # #     print("your name isn't on the list")
+# # #     print("try again")
+# # #     name=input{"what is your name?"}
 
-# # print("you can come in")
+# # # print("you can come in")
 
-# def light_switch():
-#     if light:
-#         print("Whoa! It's bright in here")
-#         light = False
+# # def light_switch():
+# #     if light:
+# #         print("Whoa! It's bright in here")
+# #         light = False
+# #     else:
+# #         print("Who turned out the lights?")
+# #         light = True
+# # light_switch()
+# # light_switch()
+
+# # even_nums =[2,4,6,8,10]
+
+# # even_nums.append(12)
+# # even_nums.insert(0,0)
+
+# # print(even_nums)
+
+# # odd_nums =(1,3,5,7,9)
+# # odd_nums.append(11)
+# # print(odd_nums)
+
+# from operator import truediv
+# from random import random
+
+
+# fav_song = [
+#     "Ram Riddlz- Hey Mr Ramrod",
+#     "Drake- Let's call it off",
+#     "Abba- SOS"
+# ]
+# print(fav_song[1])
+
+
+# num= random.randint(1,50)
+
+# while num%2==0:
+#     print("we like even number! Another!")
+#     num= random.randint(1,50)
+
+# print("Oh no! an odd number!")
+# # -------------------------------------
+# while True:
+#     num=random.randint (1,50)
+#     print(num)
+#     if num%2==0:
+#         print"we like even numbers! Another!")
+#         continue
 #     else:
-#         print("Who turned out the lights?")
-#         light = True
-# light_switch()
-# light_switch()
+# #         print("an odd number!")
+# #         break
 
-# even_nums =[2,4,6,8,10]
+# def variant():
+#     num = input("Please input a number. \n")
+#     try:
+#         print(f"Success! {int(num)} has been converted into an integer.")
+#     except:
+#         print("Try again.")
+#         variant(1)
 
-# even_nums.append(12)
-# even_nums.insert(0,0)
-
-# print(even_nums)
-
-# odd_nums =(1,3,5,7,9)
-# odd_nums.append(11)
-# print(odd_nums)
-
-from operator import truediv
-from random import random
+# variant()
 
 
-fav_song = [
-    "Ram Riddlz- Hey Mr Ramrod",
-    "Drake- Let's call it off",
-    "Abba- SOS"
-]
-print(fav_song[1])
+# import random
 
 
-num= random.randint(1,50)
+# answer = input("please type in a number")
 
-while num%2==0:
-    print("we like even number! Another!")
-    num= random.randint(1,50)
-
-print("Oh no! an odd number!")
-# -------------------------------------
-while True:
-    num=random.randint (1,50)
-    print(num)
-    if num%2==0:
-        print"we like even numbers! Another!")
-        continue
-    else:
-        print("an odd number!")
-        break
+#     try:
+#     print(int{answer})
+#     print(type{int{answer}})
+#     except:
+#         print("try again")
