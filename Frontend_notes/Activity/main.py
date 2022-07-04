588b2e4bf4b5e4dfed24be48a3b0989debd7c60f
Jafar_AD@hotmail.com <Jafar_AD@hotmail.com>
2022-07-04 17:30:11 +0100

    python oriented programming

---
+++ Frontend_notes/Activity/main.py
@@ -0,0 +1,7 @@
+from person import Person
+
+old_team_leader = Person("Liam", 31, "6'7'")
+
+new_person =Person("Will", 31, "6'0'")
+
+print(new_person)
---
+++ Frontend_notes/Activity/person.py
@@ -0,0 +1,24 @@
+
+# class person():
+#     def __init__(self, person_name, age):
+#     self.name = name
+#     self.employer = "Code Nation"
+
+
+# instructor_one = Person("will")
+# instructor_two = person("Katy")
+
+# print(instructor_one.name)
+
+# print(intructor_two.employer)
+
+class Person():
+    def __init__(self, person_name, person_age,person_height):
+        self.name = person_name
+        self.age = person_age
+        self.height = person_height 
+
+
+#! Inheritance
+#? encapsukatuin
+#*polymorphism
